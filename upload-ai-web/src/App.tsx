e87e17e4b4e028dd598df357da23ad561cc3a6fa
carolina higo <maria.prado.ext@dasa.com.br>
2023-09-13 20:40:04 -0300

    refactor(web): separate VideoInputForm as a component

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,10 +1,17 @@
-import { Github, FileVideo, Upload, Wand2 } from "lucide-react"
-import { Button } from "./components/ui/button";
-import { Separator } from "./components/ui/separator";
-import { Textarea } from "./components/ui/textarea";
-import { Label } from "./components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
-import { Slider } from "./components/ui/slider";
+import { Github, Wand2 } from "lucide-react"
+import { Button } from "./components/ui/button"
+import { Separator } from "./components/ui/separator"
+import { Textarea } from "./components/ui/textarea"
+import { Label } from "./components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select"
+import { Slider } from "./components/ui/slider"
+import { VideoInputForm } from "./components/video-input-form"
 
 export function App() {
   return (
@@ -29,49 +36,23 @@ export function App() {
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea className="" placeholder="Inclua o prompt para a IA..." />
             <Textarea
-              className="" 
-              placeholder="Inclua o prompt para a IA..."
-            />
-            <Textarea
-              className="resize-none p-4 leading-relaxed" 
+              className="resize-none p-4 leading-relaxed"
               placeholder="Resultado gerado pela IA..."
               readOnly
             />
           </div>
 
           <p className="text-sm text-muted-foreground">
-            Lembre-se: você pode utilizar a variável <code className="text-yellow-400">{'{transcription}'}</code> no seu prompt para adicionar a transcrição do vídeo selecionado.
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-yellow-400">{"{transcription}"}</code> no seu
+            prompt para adicionar a transcrição do vídeo selecionado.
           </p>
         </div>
 
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              <FileVideo className="w4 h-4" />
-              Selecione um vídeo
-            </label>
-            <input type="file" id="video" accept="video/mp4" className="sr-only" />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavras-chave mencionadas no vídeo, separadas por vírgula (,)"
-              />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Carregar vídeo
-              <Upload className="w-4 h4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
@@ -85,7 +66,9 @@ export function App() {
 
                 <SelectContent>
                   <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
+                  <SelectItem value="description">
+                    Descrição do YouTube
+                  </SelectItem>
                 </SelectContent>
               </Select>
 
@@ -115,14 +98,11 @@ export function App() {
 
             <div className="space-y-4">
               <Label>Temperatura</Label>
-              <Slider
-                min={0}
-                max={1}
-                step={0.1}
-              />
+              <Slider min={0} max={1} step={0.1} />
 
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
-                Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros 
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros
               </span>
             </div>
 
